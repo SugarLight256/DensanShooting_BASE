175ce57323911e2f8011c9f015d62788bcca698a
SugarLight256 <nippen12@gmail.com>
2016-10-14 16:28:13 +0900

    add Boss weapon and preBullet(wait until complete)

---
+++ Assets/scripts/BossWeapon.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossWeapon : MonoBehaviour {
+
+    [SerializeField]
+    private float coolTime;
+    [SerializeField]
+    private float coolMax;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        tryFire();
+    }
+
+    private void tryFire()
+    {
+        coolTime -= Time.deltaTime;
+        if(coolTime <= 0)
+        {
+            fire();
+            coolTime = coolMax;
+        }
+    }
+
+    //弾発射
+    private void fire()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+
+}
